8567d451a967e1ef4e89d22748677970df4e074b
Kirill Denisevitch <denviko00@gmail.com>
2022-11-19 21:00:08 +0300

    Example5

---
+++ Example005_ConditionalIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("What's your name?: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "kirill")
+{
+  Console.WriteLine("Hi Bro! Nice to meet you!");
+}
+else
+{
+  Console.Write("Hello! What do you want? ");
+  Console.WriteLine(username);
+  }
