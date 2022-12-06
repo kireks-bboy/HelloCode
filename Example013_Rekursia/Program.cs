751d8d5487f503fe3f4ed5f416e004690133d241
Kirill Denisevitch <denviko00@gmail.com>
2022-12-06 21:43:23 +0300

    fibonacci

--- Example013_Rekursia/Program.cs
+++ Example013_Rekursia/Program.cs
@@ -126,16 +126,39 @@
 
 
 
-double Factorial (int n)
+// double Factorial (int n)
+// {
+//   //1! = 1
+//   //0! = 1
+//   if(n == 1) return 1;
+//   else return n * Factorial(n-1);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+//   System.Console.WriteLine($"{i!} = {Factorial(i)}"); 
+// }
+
+
+
+
+
+
+
+
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+double Fibonacci(int n)
+
 {
-  //1! = 1
-  //0! = 1
-  if(n == 1) return 1;
-  else return n * Factorial(n-1);
+  if(n == 1 || n == 2) return 1;
+  else return Fibonacci(n-1) + Fibonacci(n-2);
 }
-for (int i = 1; i < 40; i++)
+for (int i = 1; i < 50; i++)
 {
-  System.Console.WriteLine($"{i!} = {Factorial(i)}"); 
+  System.Console.WriteLine($"f({i}) = {Fibonacci(i)}");
 }
 
 
